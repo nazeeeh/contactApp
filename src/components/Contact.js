6b47405e8cb8f7058ef21ad9468dc33a543679d4
Nazih <nazihahmed55@gmail.com>
2023-07-13 15:35:21 +0100

    added a bind method

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 
 class Contact extends Component {
   state = {}
-  onShowClick = e => {
-    console.log(e.target)
+  onShowClick = (name, e) => {
+    console.log(name)
   }
   
   render() {
@@ -12,7 +12,7 @@ class Contact extends Component {
 
     return (
       <div className='card card-body mb-3'>
-        <h2>{name} <i onClick={this.onShowClick} className="fa-solid fa-sort-down"></i></h2>
+        <h2>{name} <i onClick={this.onShowClick.bind(this, name)} className="fa-solid fa-sort-down"></i></h2>
         <ul className='list-group'>
           <li className='list-group-item'>{email}</li>
           <li className='list-group-item'>{phone}</li>
