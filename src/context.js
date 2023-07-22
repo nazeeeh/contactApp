0cabbfee52a04dff3e2b6ce5e434dc3abebd9d2b
Nazih <nazihahmed55@gmail.com>
2023-07-22 22:39:16 +0100

    context js re arrage

--- src/context.js
+++ src/context.js
@@ -13,8 +13,7 @@ const reducer = (state, action) => {
       case 'ADD_CONTACT':
       return {
         ...state,
-        contacts: [action.payload, ...state.contacts]
-        
+        contacts: [action.payload, ...state.contacts] 
       };
       default: 
         return state;
