830948916270922af06bcabdf09f90d9b6117eb8
Nazih <nazihahmed55@gmail.com>
2023-07-23 11:05:07 +0100

    Added axios and updated the delete function

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import axios from "axios";
 
 const Context = React.createContext()
 
@@ -26,7 +27,15 @@ export class Provider extends Component {
          dispatch: action => 
           this.setState(state => reducer(state, action))
     
-        };
+    };
+
+    componentDidMount() {
+      axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => this.setState({
+        contacts: res.data
+      }))
+    }
+    
 
         render() {
             return (
